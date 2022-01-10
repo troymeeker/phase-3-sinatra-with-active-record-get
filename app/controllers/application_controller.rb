58c86b6ec9ca00d79c018b93f0e8ec6f926551f8
troymeeker <troymeeker1@gmail.com>
2022-01-10 15:43:37 -0800

    commit tree a79f5d57fbe315e489ac2059b41e0a758ccefc99

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    #get all games from database
+    #return a JSON resp w arr of all the game data
+    games = Game.all.order(:title)
+    games.to_json
+  end
+
+  get '/games/:id' do 
+    game = Game.find(params[:id])
+    game.to_json(include: {reviews: {include: :user} })
   end
 
 end
